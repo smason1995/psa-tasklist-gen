e6e8699bc7b9905fbc519f5099cd9c4f7c9e94c2
Sydney Mason <smason516@outlook.com>
2026-01-29 04:11:37 -0800

    Removed `use` import from React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import { save } from '@tauri-apps/plugin-dialog';
 import "./App.css";
